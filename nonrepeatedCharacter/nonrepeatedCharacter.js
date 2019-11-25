b4d69b18e9abbd90e5ed0f9eab6fbb7d2c2d5649
unknown <linabensalem91@gmail.com>
2019-11-25 10:08:38 +0100

    first toy

--- nonrepeatedCharacter/nonrepeatedCharacter.js
+++ nonrepeatedCharacter/nonrepeatedCharacter.js
@@ -7,5 +7,43 @@
  */
 
 var firstNonRepeatedCharacter = function(string) {
-  // TODO: your solution here
+	var arr = string.split('');//[A,A,C,C,B,D,B ]
+	var narr = string.split('');//[A,A,C,C,B,D,B ]
+	var obj = {};
+
+
+
+  for (var i = 0;i<narr.length; i++){
+
+   for(var j=0; j<arr.length; j++){
+
+   	     obj[narr[i]] = 1;
+
+   		if(narr[i] === arr[j] ){
+   	
+   	  obj[narr[i]] =  obj[narr[i]] + 1;
+
+  		//arr.splice(arr.indexOf(arr[j]), 1); 
+  	     arr.splice(i, 1)
+  	      i++;
+  		
+  	// return obj;
+  		
+  	}
+
+   }
+
+
+  }
+
+for (var key in obj)
+{
+
+ if(obj[key] === 1){
+ 	return key;
+
+ }
+}
 };
+
+
